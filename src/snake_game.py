63f811928b521bbf6f5b42a144c4a18781529235
Felix Falk <felix.falk2903@gmail.com>
2020-12-29 00:43:15 +0100

    cleaned up the code a bit

--- src/snake_game.py
+++ src/snake_game.py
@@ -12,48 +12,37 @@ from sketch import Sketch
 delay = 0.001
 start_tail = 1
 score = 0
-
 # Set up the screen
 screen = turtle.Screen()
 screen.setup(width=1000, height=600)
 
 # SNAKE PART
-
 # Snake head
 snake = Snake()
 snakehead = snake.snakehead()
-
 # long border
 bor = Border()
 border = bor.setUpBorder()
-
 # Snake tail
 segments = []
-
 # Paddle
 # You can move the paddle with 'z' and 's'
 pad = Paddle()
 paddle = pad.paddle()
-
 # Ball of circle shape
 Ball = Ball()
 Ball.setupBall()
 
 # Global part
-
 #Sets key bindings to the right thing.
 bindings = Bindings(screen, snakehead, paddle)
 bindings.Keyboard_bindings()
-
 #movement of the snakehead
 move = Move()
 move.move(snakehead)
-
 tail = Tail()
 semgents = tail.add_tail(segments)
-
 # Displays the score
-
 sk = Sketch()
 sketch = sk.Setup()
 
@@ -63,12 +52,8 @@ while True:
     screen.update()
     #updates the scoreboard
     sk.write(score)
-    # Ball movement
-
     Ball.movementBall()
-
     # sets the start tail to three
-
     tail.start_tail(segments)
 
     # Move the end segments first in reverse order
@@ -84,9 +69,6 @@ while True:
         segments[0].goto(x, y)
 
     move.move(snakehead)
-
-    # ALL NEXT CODE ARE COLLISIONS
-
     # Collision detection with borders
     if snakehead.xcor() > 490 or snakehead.xcor() < -350 or snakehead.ycor() > 290 or snakehead.ycor() < -290:
         time.sleep(0.5)
@@ -144,13 +126,10 @@ while True:
 
     # Collision of the ball with the paddle
     Ball.checkCollitionWithPeddal(paddle)
-
     # Collision with ball and the tail of the snake
     # When you go with your snake through your ball or go sideways the ball goes back in reverse way
     # Sometimes when you go through the ball, this next code is executed twice because it makes two contacts with the snake
-
     Ball.checkCollitionWithTail(segments)
-
     time.sleep(delay)
 
 screen.mainloop()
